a2cd6a92ab780fcb968c37cdf097a82dddcab66f
jloc8749 <jloc8749@uni.sydney.edu.au>
2016-03-21 14:41:03 +1100

    attempt at some wk2 tut hw

---
+++ lab/src/lab/ex2Triangle.java
@@ -0,0 +1,22 @@
+package lab;
+
+public class ex2Triangle {
+
+	public static void main(String[] args) {
+		// make a triangle print from numbers
+		int num = Integer.parseInt(args[0]);
+		double d = 1;
+		
+		while (d <= num){
+			//each number counting up on the same line cat ' '.
+			System.out.print(d + " ");
+			d++; //when num reached
+					
+			
+			
+		}
+		System.out.println("");
+
+	}
+
+}
---
+++ lab/src/lab/tutorial_2_hw.java
@@ -0,0 +1,28 @@
+package lab;
+
+public class tutorial_2_hw {
+
+	public static void main(String[] args) {
+		// hw assignment from wk 3 tutorial
+		String grades = "HDCPF";
+		String input = args[0];
+		var = input.equalsIgnoreCase(args[0]);
+		final char a = grades.charAt(0);
+		
+		switch (var){
+		
+		case a:
+				System.out.println(grades.charAt(0));
+			break;
+		}
+		
+		
+		
+	}
+		}
+			
+		
+
+	
+
+
